e48b078ed643de59f1fdcef205c8aae69e9631df
RojenSoliman <rojen.soliman@lpunetwork.edu.ph>
2025-07-03 22:34:46 +0800

    working connection to the database

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php 
 //This is the database configuration file for the EducAid system.
-    $connection = pg_connect("host=127.0.0.1 dbname=educaid user=postgres password=postgres_dev_2025");
+    $connection = pg_connect("host=localhost dbname=educaid user=postgres password=postgres_dev_2025");
     if (!$connection) {
         die("Connection failed: " . pg_last_error());
     }
