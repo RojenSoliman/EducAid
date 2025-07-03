f442a3e5862430cb58c164324b29e6dfe1d15e2b
RojenSoliman <rojen.soliman@lpunetwork.edu.ph>
2025-07-03 23:18:12 +0800

    added more testing for database connection at read

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    include 'config/database.php';
+    // Database connection can be tested here if needed
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -16,9 +20,52 @@
             document.getElementById('testButton').addEventListener('click', function() {
                 alert('Button was clicked!');
             });
-
-
         </script>
-        <a href="config/database.php" target="_blank">Open db connection test</a>
+
+        <?php
+            $result = pg_query($connection, "SELECT * FROM users");
+            if(!$result){
+                echo "An error occurred while querying the database.";
+            } else {
+                echo "<h2>Database Query Result:</h2>";
+                echo "<ul>";
+                while ($row = pg_fetch_assoc($result)) {
+                    echo "<li>" . htmlspecialchars($row['user_fname']) . "</li>";
+                }
+                echo "</ul>";
+            }
+        ?>
+
+        <br>
+        <div
+            class="table-responsive"
+        >
+            <table
+                class="table table-primary"
+            >
+                <thead>
+                    <tr>
+                        <th scope="col">Column 1</th>
+                        <th scope="col">Column 2</th>
+                        <th scope="col">Column 3</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td scope="row">R1C1</td>
+                        <td>R1C2</td>
+                        <td>R1C3</td>
+                    </tr>
+                    <tr class="">
+                        <td scope="row">Item</td>
+                        <td>Item</td>
+                        <td>Item</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        
+        
+        <a href="config/database.php">Open db connection test</a>
     </body>
 </html>
