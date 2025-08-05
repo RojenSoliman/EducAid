aa6366f9e611876c7c777eeeed07faac5c532486
RojenSoliman <rojen.soliman@lpunetwork.edu.ph>
2025-08-05 18:57:07 +0800

    Updated logout so it aint annoying

--- modules/admin/logout.php
+++ modules/admin/logout.php
@@ -1,7 +1,29 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
+
+// Only unset admin-specific session variables
+unset($_SESSION['admin_id']);
+unset($_SESSION['admin_username']);
+
+// Remove any admin-specific temporary variables
+$adminKeys = array_filter(array_keys($_SESSION), function($key) {
+    return strpos($key, 'admin_') === 0;
+});
+
+foreach ($adminKeys as $key) {
+    unset($_SESSION[$key]);
+}
+
+// Clear any shared login/OTP variables (since user is logging out)
+unset($_SESSION['login_otp']);
+unset($_SESSION['login_otp_time']);
+unset($_SESSION['login_pending']);
+unset($_SESSION['forgot_otp']);
+unset($_SESSION['forgot_otp_time']);
+unset($_SESSION['forgot_otp_email']);
+unset($_SESSION['forgot_otp_role']);
+unset($_SESSION['forgot_otp_verified']);
+
 header("Location: ../../unified_login.php");
 exit;
 ?>
--- modules/student/logout.php
+++ modules/student/logout.php
@@ -1,7 +1,33 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
+
+// Only unset student-specific session variables
+unset($_SESSION['student_id']);
+unset($_SESSION['student_username']);
+unset($_SESSION['schedule_modal_shown']);
+
+// Remove any student-specific temporary variables
+$studentKeys = array_filter(array_keys($_SESSION), function($key) {
+    return strpos($key, 'student_') === 0 || 
+           strpos($key, 'profile_') === 0 || 
+           strpos($key, 'upload_') === 0 ||
+           strpos($key, 'qr_codes') === 0;
+});
+
+foreach ($studentKeys as $key) {
+    unset($_SESSION[$key]);
+}
+
+// Clear any shared login/OTP variables (since user is logging out)
+unset($_SESSION['login_otp']);
+unset($_SESSION['login_otp_time']);
+unset($_SESSION['login_pending']);
+unset($_SESSION['forgot_otp']);
+unset($_SESSION['forgot_otp_time']);
+unset($_SESSION['forgot_otp_email']);
+unset($_SESSION['forgot_otp_role']);
+unset($_SESSION['forgot_otp_verified']);
+
 header("Location: ../../unified_login.php");
 exit;
 ?>
