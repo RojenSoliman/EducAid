75488968bdfcba81b81fd733c41cc32177ecb8a1
Reb Nigoza <reb.nigoza@lpunetwork.edu.ph>
2025-10-05 11:23:17 +0800

    update: altered the table to add the city logos

--- sql/alter_municipalities_add_logo_fields.sql
+++ sql/alter_municipalities_add_logo_fields.sql
@@ -18,7 +18,7 @@ SET name              = 'City of General Trias',
     slug              = 'general-trias',
     lgu_type          = 'city',
     district_no       = 6,
-    preset_logo_image = '/assets/lgus/general-trias/logo.png'
+  preset_logo_image = '/assets/City Logos/General_Trias_City_Logo.png'
 WHERE municipality_id = 1;
 
 BEGIN;
@@ -26,13 +26,13 @@ BEGIN;
 -- CITIES (IDs fixed; 1 is Gentri)
 INSERT INTO municipalities (municipality_id, name, slug, lgu_type, district_no, preset_logo_image)
 VALUES
-  (2, 'City of Dasmariñas',  'dasmarinas',     'city', 4, '/assets/lgus/dasmarinas/logo.png'),
-  (3, 'City of Imus',        'imus',           'city', 3, '/assets/lgus/imus/logo.png'),
-  (4, 'City of Bacoor',      'bacoor',         'city', 2, '/assets/lgus/bacoor/logo.png'),
-  (5, 'Cavite City',         'cavite-city',    'city', 1, '/assets/lgus/cavite-city/logo.png'),
-  (6, 'Trece Martires City', 'trece-martires', 'city', 7, '/assets/lgus/trece-martires/logo.png'),
-  (7, 'Tagaytay City',       'tagaytay',       'city', 8, '/assets/lgus/tagaytay/logo.png'),
-  (8, 'City of Carmona',     'carmona',        'city', 5, '/assets/lgus/carmona/logo.png')
+  (2, 'City of Dasmariñas',  'dasmarinas',     'city', 4, '/assets/City Logos/Dasma_City_Logo.png'),
+  (3, 'City of Imus',        'imus',           'city', 3, '/assets/City Logos/Imus_City_Logo.png'),
+  (4, 'City of Bacoor',      'bacoor',         'city', 2, '/assets/City Logos/Bacoor_City_Logo.png'),
+  (5, 'Cavite City',         'cavite-city',    'city', 1, '/assets/City Logos/Cavite_City_Logo.png'),
+  (6, 'Trece Martires City', 'trece-martires', 'city', 7, '/assets/City Logos/Trece_Martires_City_Logo.png'),
+  (7, 'Tagaytay City',       'tagaytay',       'city', 8, '/assets/City Logos/Tagaytay_City_Logo.png'),
+  (8, 'City of Carmona',     'carmona',        'city', 5, '/assets/City Logos/Carmona_Cavite_Logo.png')
 ON CONFLICT (municipality_id) DO UPDATE
 SET name = EXCLUDED.name,
     slug = EXCLUDED.slug,
@@ -43,21 +43,21 @@ SET name = EXCLUDED.name,
 -- MUNICIPALITIES (keep 101–115)
 INSERT INTO municipalities (municipality_id, name, slug, lgu_type, district_no, preset_logo_image)
 VALUES
-  (101, 'Kawit',                    'kawit',                    'municipality', 1, '/assets/lgus/kawit/logo.png'),
-  (102, 'Noveleta',                 'noveleta',                 'municipality', 1, '/assets/lgus/noveleta/logo.png'),
-  (103, 'Rosario',                  'rosario',                  'municipality', 1, '/assets/lgus/rosario/logo.png'),
-  (104, 'General Mariano Alvarez',  'general-mariano-alvarez',  'municipality', 5, '/assets/lgus/general-mariano-alvarez/logo.png'),
-  (105, 'Silang',                   'silang',                   'municipality', 5, '/assets/lgus/silang/logo.png'),
-  (106, 'Amadeo',                   'amadeo',                   'municipality', 7, '/assets/lgus/amadeo/logo.png'),
-  (107, 'Indang',                   'indang',                   'municipality', 7, '/assets/lgus/indang/logo.png'),
-  (108, 'Tanza',                    'tanza',                    'municipality', 7, '/assets/lgus/tanza/logo.png'),
-  (109, 'Alfonso',                  'alfonso',                  'municipality', 8, '/assets/lgus/alfonso/logo.png'),
-  (110, 'General Emilio Aguinaldo', 'general-emilio-aguinaldo', 'municipality', 8, '/assets/lgus/general-emilio-aguinaldo/logo.png'),
-  (111, 'Magallanes',               'magallanes',               'municipality', 8, '/assets/lgus/magallanes/logo.png'),
-  (112, 'Maragondon',               'maragondon',               'municipality', 8, '/assets/lgus/maragondon/logo.png'),
-  (113, 'Mendez-Nuñez',             'mendez-nunez',             'municipality', 8, '/assets/lgus/mendez-nunez/logo.png'),
-  (114, 'Naic',                     'naic',                     'municipality', 8, '/assets/lgus/naic/logo.png'),
-  (115, 'Ternate',                  'ternate',                  'municipality', 8, '/assets/lgus/ternate/logo.png')
+  (101, 'Kawit',                    'kawit',                    'municipality', 1, '/assets/City Logos/Kawit_Logo.png'),
+  (102, 'Noveleta',                 'noveleta',                 'municipality', 1, '/assets/City Logos/Noveleta_Logo.png'),
+  (103, 'Rosario',                  'rosario',                  'municipality', 1, '/assets/City Logos/Rosario_Logo.jpg'),
+  (104, 'General Mariano Alvarez',  'general-mariano-alvarez',  'municipality', 5, '/assets/City Logos/General_Mariano_Alvarez_Logo.png'),
+  (105, 'Silang',                   'silang',                   'municipality', 5, '/assets/City Logos/Silang_City_Logo.png'),
+  (106, 'Amadeo',                   'amadeo',                   'municipality', 7, '/assets/City Logos/Amadeo_Logo.png'),
+  (107, 'Indang',                   'indang',                   'municipality', 7, '/assets/City Logos/Indang_Logo.png'),
+  (108, 'Tanza',                    'tanza',                    'municipality', 7, '/assets/City Logos/Tanza,_Cavite_Logo.png'),
+  (109, 'Alfonso',                  'alfonso',                  'municipality', 8, '/assets/City Logos/Alfonso_Logo.png'),
+  (110, 'General Emilio Aguinaldo', 'general-emilio-aguinaldo', 'municipality', 8, '/assets/City Logos/Gen_Emilio_Aguinaldo_Logo.png'),
+  (111, 'Magallanes',               'magallanes',               'municipality', 8, '/assets/City Logos/Magallanes_Logo.png'),
+  (112, 'Maragondon',               'maragondon',               'municipality', 8, '/assets/City Logos/Maragondon_Logo.png'),
+  (113, 'Mendez-Nuñez',             'mendez-nunez',             'municipality', 8, '/assets/City Logos/Mendez_Logo.png'),
+  (114, 'Naic',                     'naic',                     'municipality', 8, '/assets/City Logos/Naic_Logo.png'),
+  (115, 'Ternate',                  'ternate',                  'municipality', 8, '/assets/City Logos/Ternate_Logo.png')
 ON CONFLICT (municipality_id) DO UPDATE
 SET name = EXCLUDED.name,
     slug = EXCLUDED.slug,
